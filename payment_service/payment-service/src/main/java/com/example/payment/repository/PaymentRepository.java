5dd8bcfca55cd4db0a0507d7c8bbbf82033b50eb
Hanna Sharma <150518039+HannaSharma05@users.noreply.github.com>
2026-07-08 15:13:31 +0530

    Add files via upload

---
+++ payment_service/payment-service/src/main/java/com/example/payment/repository/PaymentRepository.java
@@ -0,0 +1,11 @@
+package com.example.payment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.payment.Payment;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+}
